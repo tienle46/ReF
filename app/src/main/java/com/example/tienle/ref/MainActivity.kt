849c5e5b7398cbb956d65e30228d56ea75d82480
Tiến Lê <tienle4695@gmail.com>
2018-10-01 23:49:57 +0300

    add location to weather activity

--- app/src/main/java/com/example/tienle/ref/MainActivity.kt
+++ app/src/main/java/com/example/tienle/ref/MainActivity.kt
@@ -1,11 +1,18 @@
 package com.example.tienle.ref
 
+import android.annotation.SuppressLint
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.location.Location
 import android.os.AsyncTask
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v4.app.ActivityCompat
+import android.support.v4.content.ContextCompat
 import android.util.Log
 import android.widget.Toast
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.tasks.OnCompleteListener
 import kotlinx.android.synthetic.main.activity_main.*
 import org.json.JSONObject
 import java.io.BufferedReader
@@ -13,6 +20,9 @@ import java.io.InputStreamReader
 import java.net.URL
 
 class MainActivity : AppCompatActivity() {
+
+
+    private var mPermissionGranted: Boolean = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         val apiKey: String = getString(R.string.weather_api_key)
@@ -23,10 +33,22 @@ class MainActivity : AppCompatActivity() {
             val intent: Intent = Intent(this, MapsActivity::class.java)
             startActivity(intent)
         }
+        locationPermission()
 
         val urlJSON = "http://api.openweathermap.org/data/2.5/weather?q=London&appid=${apiKey}&units=Imperial"
         getWeatherInfo().execute(urlJSON)
 
+        getUserLocation {
+            Toast.makeText(applicationContext,"lat: ${it.latitude}",Toast.LENGTH_LONG).show()
+        }
+
+//        val userLocation = getUserLocation()
+//
+//        if(userLocation != null) {
+//            Toast.makeText(applicationContext,"lat: ${userLocation.latitude}",Toast.LENGTH_LONG).show()
+//        } else {
+//            Toast.makeText(applicationContext,"null cmmr",Toast.LENGTH_LONG).show()
+//        }
     }
 
     inner class getWeatherInfo: AsyncTask<String, Void, String?>() {
@@ -53,7 +75,6 @@ class MainActivity : AppCompatActivity() {
 
         override fun onPostExecute(result: String?) {
             super.onPostExecute(result)
-//            Toast.makeText(applicationContext,result,Toast.LENGTH_LONG)
             handleWeatherInfo(result)
         }
 
@@ -62,8 +83,51 @@ class MainActivity : AppCompatActivity() {
     fun handleWeatherInfo(data: String?) {
         val weatherObject:JSONObject = JSONObject(data)
         val temp = weatherObject.getJSONObject("main").getString("temp")
-        Toast.makeText(applicationContext,temp,Toast.LENGTH_LONG).show()
+        //Toast.makeText(applicationContext,temp,Toast.LENGTH_LONG).show()
 
     }
+
+    private fun locationPermission() {
+        if (ContextCompat.checkSelfPermission(applicationContext,android.Manifest.permission.ACCESS_FINE_LOCATION)
+            == PackageManager.PERMISSION_GRANTED) {
+            mPermissionGranted = true
+        } else {
+            val permission = arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION)
+            ActivityCompat.requestPermissions(this, permission,0)
+        }
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        mPermissionGranted = false
+        when(requestCode) {
+            0 -> {
+                if (grantResults.size > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    mPermissionGranted = true
+                }
+            }
+        }
+    }
+
+    private fun getUserLocation(callback: (Location) -> Unit) {
+        val mFusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this)
+        var mLastKnownLocation: Location
+        try {
+            if(mPermissionGranted) {
+                val locationResult = mFusedLocationProviderClient.lastLocation
+                Log.d("wtf", locationResult.toString())
+
+                locationResult.addOnCompleteListener(this@MainActivity, OnCompleteListener {
+                    if(it.isSuccessful) {
+                        mLastKnownLocation = it.result
+                        callback(mLastKnownLocation)
+                    }
+                })
+            }
+        } catch (e: SecurityException) {
+            Log.e("dmm", e.message)
+        }
+    }
+
 }
 
